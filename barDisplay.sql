bbec7b0fc3ba808fc86c4236c0e3dbf56567867f
Ronny <52419137+stealth14@users.noreply.github.com>
2020-02-19 13:58:22 -0500

    Add files via upload

---
+++ barDisplay.sql
@@ -0,0 +1,11 @@
+SELECT * from customer
+SELECT * from usuarios
+SELECT * from combos
+select * from productos
+SELECT * from ordenes
+select * from mesas
+
+
+select * from productos
+
+delete from productos
